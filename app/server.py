3c1e3f1f65f3fea3d6076b4f1430a1db1973cfb1
Gliverson01 <gliversonfrp@gmail.com>
2024-04-18 15:08:21 -0300

    message alterada na main

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def frontpage():
-    message= "Hello, Dev Guru2!"
+    message= "Hello, Dev Guru3!"
     html_table = generate_html_table(num_rows=5)
     return render_template('frontpage.html', message=message, table=html_table)
 
